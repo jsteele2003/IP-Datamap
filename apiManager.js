a763ff83100899b56d884f517a55f7e9041b5075
Joe Steele <steej991@newschool.edu>
2017-09-24 18:31:12 +0000

    animated arcs

--- apiManager.js
+++ apiManager.js
@@ -1,6 +1,33 @@
 var request = require('request'); 
-var async = require('async'); 
-var d3 = require('d3');
-var csv = require('csv');
-var TSV = require('tsv');
+var fs = require('fs');
+var csv = require("csv");
+var parse = require('csv-parse');
 
+var ipArcs = [];
+var origins = [{latitude: "51.5092", longitude : "-0.0955" }, 
+               {latitude:"35.685" , longitude: "139.7514"},
+               {latitude :"40.6614", longitude: "-73.9583"}]
+
+var parser = parse(function(err, data){
+    var apiRequest = "http://freegeoip.net/json/" + data[1];
+    var thisArc = new Object;
+    thisArc.site = data[1];
+    
+      request(apiRequest, function requestCallback(err, resp, body) {
+          if (err) {throw err;}
+          
+        //console.log(JSON.parse(body));
+          thisArc.destination = {
+              latitude : JSON.parse(body).latitude,
+              longitude : JSON.parse(body).longitude,
+          }
+          console.log(thisArc);
+      });
+//   data.forEach(function(element){
+//       console.log(element);
+//   })
+
+    
+});
+
+fs.createReadStream('data/uniqueUrls.csv').pipe(parser);
