8b01f2c31bc3fd01f6a21716d3a54819b5382a26
Joe Steele <steej991@newschool.edu>
2017-09-24 21:44:10 +0000

    animation of browser history json

--- apiManager.js
+++ apiManager.js
@@ -2,31 +2,42 @@ var request = require('request');
 var fs = require('fs');
 var csv = require("csv");
 var parse = require('csv-parse');
+var async = require('async');
 
 var ipArcs = [];
-var origins = [{latitude: "51.5092", longitude : "-0.0955" }, 
-               {latitude:"35.685" , longitude: "139.7514"},
-               {latitude :"40.6614", longitude: "-73.9583"}]
+var originNames = ["UK", "Japan", "NY"];
+var origins = [{latitude: 51.5092, longitude : -0.0955 }, //UK 
+               {latitude:35.685 , longitude: 139.7514}, //Japan
+               {latitude :40.6614, longitude: -73.9583}] //NY
 
 var parser = parse(function(err, data){
-    var apiRequest = "http://freegeoip.net/json/" + data[1];
-    var thisArc = new Object;
-    thisArc.site = data[1];
-    
-      request(apiRequest, function requestCallback(err, resp, body) {
-          if (err) {throw err;}
-          
-        //console.log(JSON.parse(body));
-          thisArc.destination = {
-              latitude : JSON.parse(body).latitude,
-              longitude : JSON.parse(body).longitude,
-          }
-          console.log(thisArc);
-      });
-//   data.forEach(function(element){
-//       console.log(element);
-//   })
-
+     async.eachSeries(data, function asyncIteratee(value, callback) {
+        //console.log(value);
+        var apiRequest = "http://freegeoip.net/json/" + value;
+        var thisArc = new Object;
+        thisArc.site = value[0];
+        oIndex = Math.floor(Math.random()*origins.length);
+        thisArc.origin = origins[oIndex];
+        thisArc.country = originNames[oIndex];
+        
+          request(apiRequest, function requestCallback(err, resp, body) {
+              if (err) {throw err;}
+              
+            //console.log(JSON.parse(body));
+              thisArc.destination = {
+                  latitude : JSON.parse(body).latitude,
+                  longitude : JSON.parse(body).longitude,
+              }
+              //console.log(thisArc);
+              ipArcs.push(thisArc);
+          });
+        
+        setTimeout(callback, 50); 
+     }, function asyncCallback(){
+        //console.log(ipArcs);
+        var myJsonString = JSON.stringify(ipArcs);
+        console.log(myJsonString);
+    })
     
 });
 
